9128ccca713ae1b025303fd666c71113f286101f
Titova Sophia <sof_titova@rambler.ru>
2017-05-02 20:36:25 +0300

    Update BB.kt

--- JB/kotlin/BB.kt
+++ JB/kotlin/BB.kt
@@ -1,25 +1,42 @@
-import java.io.*
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.io.PrintWriter
 
 internal var ans: Long = 0
 internal var powTen = LongArray(19)
 internal var Ones = LongArray(19)
 
+
+fun main(args: Array<String>) {
+    val reader = BufferedReader(FileReader("input.txt"))
+    val writer = PrintWriter(File("output.txt"))
+    val n = reader.readLine().toInt()
+    powTen[0] = 1
+    Ones[0] = 1
+    powers()
+    for (i in 0..n - 1) {
+        ans = 0
+        count(reader.readLine(), 0)
+        writer.println("Case #" + (i + 1) + ": " + ans)
+    }
+    writer.close()
+}
+
 fun count(strVal: String, i: Int) {
     if (i == strVal.length) {
         return
     }
-    val `val` = java.lang.Long.parseLong(strVal)
+    val value = strVal.toLong()
     var tmp = Ones[strVal.length - i - 1]
-
     var res: Long = 0
     var ii = 0
-    while (ans + tmp <= `val` && ii < 9) {
+    while (ans + tmp <= value && ii < 9) {
         res = tmp
         ii++
         tmp += Ones[strVal.length - i - 1]
     }
-    ans += res
-    ans = ans / powTen[strVal.length - i - 1] * powTen[strVal.length - i - 1]
+    ans = (ans + res) / powTen[strVal.length - i - 1] * powTen[strVal.length - i - 1]
     count(strVal, i + 1)
 }
 
@@ -30,19 +47,4 @@ fun powers() {
     }
 }
 
-fun main(args: Array<String>) {
-    val `in` = BufferedReader(FileReader("input.txt"))
-    val out = PrintWriter(File("output.txt"))
-    val n = Integer.parseInt(`in`.readLine())
-    powTen[0] = 1
-    Ones[0] = 1
-    powers()
-    for (i in 0..n - 1) {
-        ans = 0
-        val `val` = `in`.readLine()
-        count(`val`, 0)
-        out.println("Case #" + (i + 1) + ": " + ans)
-    }
-    out.close()
-}
 
