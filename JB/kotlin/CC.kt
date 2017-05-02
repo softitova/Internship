4225731152fc5650ae14008a339357b2dc27e729
Titova Sophia <sof_titova@rambler.ru>
2017-05-02 20:36:55 +0300

    Update CC.kt

--- JB/kotlin/CC.kt
+++ JB/kotlin/CC.kt
@@ -1,27 +1,38 @@
 import javafx.util.Pair
-import java.io.*
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.io.PrintWriter
+import java.lang.Long.parseLong
 import java.util.*
 
 fun main(args: Array<String>) {
-    val `in` = BufferedReader(FileReader("input.txt"))
-    val out = PrintWriter(File("output.txt"))
-    val t = Integer.parseInt(`in`.readLine())
+    val reader = BufferedReader(FileReader("input.txt"))
+    val writer = PrintWriter(File("output.txt"))
+    val t = Integer.parseInt(reader.readLine())
     var st: StringTokenizer
     for (i in 0..t - 1) {
-        st = StringTokenizer(`in`.readLine())
-        val p = count(java.lang.Long.parseLong(st.nextToken()),
-                java.lang.Long.parseLong(st.nextToken()))
-        out.println("Case #" + (i + 1) + ": " + p.key + " " + p.value);
+        st = StringTokenizer(reader.readLine())
+        val p = count(parseLong(st.nextToken()), st.nextToken().toLong())
+        writer.println("Case #" + (i + 1) + ": " + p.key + " " + p.value);
     }
-    out.close()
+    writer.close()
 }
 
 private fun count(n: Long, k: Long): Pair<Long, Long> {
-    var pow: Long = Math.pow(2.0, (Math.log10(k.toDouble()) / Math.log10(2.0)).toLong().toDouble()).toLong() - 1
-    var length: Long = (n - pow) / (pow + 1)
-    if (n - pow - length * (pow + 1) - (k - pow) >= 0) {
-        length += 1
+    var mk = k
+    val m = TreeMap<Long, Long>()
+    m.put(-n, 1L)
+
+    while (true) {
+        val p = m.firstEntry()
+        m.remove(p.key)
+        if (p.value < mk) {
+            m.merge(-(-p.key / 2), p.value) { u, v -> u + v }
+            m.merge(-((-p.key - 1) / 2), p.value) { u, v -> u + v }
+            mk -= p.value
+        } else {
+            return Pair(-p.key / 2, (-p.key - 1) / 2);
+        }
     }
-    var max : Long = length / 2
-    return Pair(max, if ((length and 1L == 0L)) max - 1 else max)
 }
